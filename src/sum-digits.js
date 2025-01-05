f7d10be8319541dc5ae76dd01f25652a254cdc8c
ann-sm <annsmirnovva@gmail.com>
2025-01-05 20:28:57 +0300

    feat: complete sum-digit and vigenere-cipher tasks

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,8 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+  const nStr = n.toString();
+  let sum = 0;
+
+  for (let i = 0; i < nStr.length; i++) {
+    sum += Number(nStr[i]);
+    if (sum.toString().length > 1) {
+      sum = Number(sum.toString()[0]) + Number(sum.toString()[1]);
+    }
+  }
+
+  return sum;
   // remove line with error and write your code here
 }
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  constructor(mode = true) {
+    this.mode = mode;
+    // this.message = message;
+    // this.key = key;
+    // this.encryptedMessage = encryptedMessage;
+  }
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      let encryptedArray = [];
+
+      message = message.toUpperCase();
+      key = key.repeat(Math.ceil(message.length / key.length)).slice(0, message.length).toUpperCase();
+  
+      for (let i = 0; i < message.length; i++) {
+        let letterIndex;
+        let shift;
+  
+        if (message[i] >= 'A' && message[i] <= 'Z') {
+          letterIndex = message.charCodeAt(i) - 'A'.charCodeAt(0);
+          shift = key.charCodeAt(i) - 'A'.charCodeAt(0);
+  
+          encryptedArray.push(String.fromCharCode('A'.charCodeAt(0) + (letterIndex + shift) % 26));
+        }
+  
+        else {
+          key = key.slice(0, i) + ' ' + key.slice(i);
+          encryptedArray.push(message[i]);
+        } 
+      }
+  
+      if (this.mode === false) {
+        encryptedArray.reverse();
+      }
+  
+      return encryptedArray.join('');
+    }
+
+
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      let decryptedArray = [];
+
+      encryptedMessage = encryptedMessage.toUpperCase();
+      key = key.repeat(Math.ceil(encryptedMessage.length / key.length)).slice(0, encryptedMessage.length).toUpperCase();
+
+      for (let i = 0; i < encryptedMessage.length; i++) {
+        let letterIndex;
+        let shift;
+
+        if (encryptedMessage[i] >= 'A' && encryptedMessage[i] <= 'Z') {
+          letterIndex = encryptedMessage.charCodeAt(i) - 'A'.charCodeAt(0);
+          shift = key.charCodeAt(i) - 'A'.charCodeAt(0);
+  
+          decryptedArray.push(String.fromCharCode('A'.charCodeAt(0) + (letterIndex - shift + 26) % 26));
+        }
+  
+        else {
+          key = key.slice(0, i) + ' ' + key.slice(i);
+          decryptedArray.push(encryptedMessage[i]);
+        } 
+      }
+
+      if (this.mode === false) {
+        decryptedArray.reverse();
+      }
+  
+      return decryptedArray.join('');
     // remove line with error and write your code here
+   }
   }
 }
 
