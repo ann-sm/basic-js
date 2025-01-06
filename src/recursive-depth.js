85409b6b538d303ce6087161792d115141e1f6e4
ann-sm <annsmirnovva@gmail.com>
2025-01-06 14:06:29 +0300

    feat: complete recursive task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,7 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
+
+
   calculateDepth(arr) {
+    let counter = 1;
+    let depth = 1;
+
+    arr.forEach(el => {
+      if (Array.isArray(el)) {
+        counter = 1 + this.calculateDepth(el);
+
+        if (depth < counter) {
+          depth = counter;
+        }
+      }
+    })
+    
+    return depth;
     // remove line with error and write your code here
   }
 }
