6e9e88ebc368b83c21c1a86b8b648781c6516b65
ann-sm <annsmirnovva@gmail.com>
2025-01-06 17:33:44 +0300

    feat: complete mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,8 +23,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  let arr = matrix.map(el => el);
+
+  if (arr.flat().includes(true)) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        let count = 1;
+  
+        if (matrix[i][j] === true) {
+          arr[i][j] = count;
+        } else {
+          if (i !== 0 && matrix[i - 1][j] === true) {
+            count += 1;
+          }
+          if (i !== arr.length - 1 && matrix[i + 1][j] === true) {
+            count += 1;
+          }
+          if (j !== 0 && matrix[i][j - 1] === true) {
+            count += 1;
+          }
+          if (j !== arr.length - 1 && matrix[i][j + 1] === true) {
+            count += 1;
+          }   
+          arr[i][j] = count; 
+        }
+      }
+    }
+    return arr;
+
+  } else {
+    let emptyArr = matrix.map((el, i) => el[i] = el.map((el, j) => el[j] = 0));
+    return emptyArr;
+  }
   // remove line with error and write your code here
 }
 
